c9ac9f510fc843d1f8e8292d558cf14030eed2db
SpinoniRaffaele <raffaelespinoni@gmail.com>
2023-11-16 09:09:00 +0100

    add builder to the model

--- src/main/java/com/rspinoni/gums/model/Project.java
+++ src/main/java/com/rspinoni/gums/model/Project.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Project {
   @Id
   String id;
