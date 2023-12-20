b3a925484107b0778055c78c1c583b85d03d7920
SpinoniRaffaele <raffaelespinoni@gmail.com>
2023-12-20 08:52:13 +0100

    fix ui tests

--- gums-ui/src/app/app.component.spec.ts
+++ gums-ui/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { GraphSectionComponent } from "./graph-section/graph-section.component";
+import { HttpClient, HttpHandler } from "@angular/common/http";
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent, GraphSectionComponent]
+    declarations: [AppComponent, GraphSectionComponent],
+    providers: [HttpClient, HttpHandler]
   }));
 
   it('should create the app', () => {
--- gums-ui/src/app/graph-section/graph-section.component.spec.ts
+++ gums-ui/src/app/graph-section/graph-section.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GraphSectionComponent } from './graph-section.component';
+import { HttpClient, HttpHandler } from "@angular/common/http";
 
 describe('GraphSectionComponent', () => {
   let component: GraphSectionComponent;
@@ -8,7 +9,8 @@ describe('GraphSectionComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [GraphSectionComponent]
+      declarations: [GraphSectionComponent],
+      providers: [HttpClient, HttpHandler]
     });
     fixture = TestBed.createComponent(GraphSectionComponent);
     component = fixture.componentInstance;
